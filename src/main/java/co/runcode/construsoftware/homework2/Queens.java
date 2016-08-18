491f7d1eb54a64b74e9ca784ef20752c24da14df
albeirobet <eaar23@hotmail.com>
2016-08-18 14:28:02 -0500

    Continuando 8 Reinas

--- src/main/java/co/runcode/construsoftware/homework2/Queens.java
+++ src/main/java/co/runcode/construsoftware/homework2/Queens.java
@@ -5,7 +5,9 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.SynchronousQueue;
 
 import javax.swing.JButton;
@@ -99,24 +101,88 @@ public class Queens extends JFrame implements Serializable {
 
 		solve.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int matrixCopy[][] = copiarMatrix(fields);
 				Solver solver = new Solver();
 				IntVar var[][] = VariableFactory.boundedMatrix("v", 8, 8, 0, 1, solver);
 				IntVar M = VariableFactory.fixed(1, solver);
-				IntVar vm1[][] = new IntVar[9][9];
-				IntVar vm2[][] = new IntVar[9][9];
-				int x = 0;
-				for (int r = 0; r < 9; r++) {
-					for (int c = 0; c < 9; c++) {
+
+				IntVar vm1[][] = new IntVar[8][8];
+				IntVar vm2[][] = new IntVar[8][8];
+				for (int r = 0; r < 8; r++) {
+					for (int c = 0; c < 8; c++) {
 						vm1[r][c] = var[r][c];
 						vm2[c][r] = var[r][c];
-						x++;
 					}
 					solver.post(ICF.sum(vm1[r], M));
 				}
 
+				for (int c = 0; c < 8; c++) {
+					solver.post(ICF.sum(vm2[c], M));
+				}
+
+				// Generando Vector dinamico diagonales Izquierda a Derecha
+				List<IntVar[]> listDiagIzqDer = getVectorDiagonalIzqDer(var);
+				for (IntVar[] diagonal : listDiagIzqDer) {
+					solver.post(ICF.sum(diagonal, M));
+				}
+
+				System.out.println(" Entrando a obtener la soluciòn a Choko");
+				solver.findSolution();
+				System.out.println(" Saliendo de la soluciòn de Choko");
+				if (solver.findSolution()) {
+					for (int r = 0; r < 8; r++) {
+						for (int c = 0; c < 8; c++) {
+							if (var[r][c].getValue() == 0) {
+								fields[r][c].setText("");
+							} else {
+								fields[r][c].setText("" + var[r][c].getValue());
+							}
+
+						}
+					}
+				}
+
 			}
 		});
+	}
+
+	public List<IntVar[]> getVectorDiagonalIzqDer(IntVar var[][]) {
+		IntVar diag1[] = new IntVar[8];
+		List<IntVar[]> listDiag = new ArrayList<IntVar[]>();
+
+		int dim = 8;
+		System.out.println();
+		int contArray = 0;
+
+		for (IntVar[] row : var)
+			System.out.println(Arrays.toString(row));
+
+		for (int k = 0; k < dim * 2; k++) {
+			for (int j = 0; j <= k; j++) {
+				int i = k - j;
+				if (i < dim && j < dim) {
+					diag1[j] = var[i][j];
+				}
+			}
+			for (int i = 0; i < diag1.length; i++) {
+				if (diag1[i] != null) {
+					contArray++;
+				}
+			}
+			IntVar diagTmp[] = new IntVar[contArray];
+			contArray = 0;
+			int posInser = 0;
+			for (int i = 0; i < diag1.length; i++) {
+				if (diag1[i] != null) {
+					diagTmp[posInser] = diag1[i];
+					posInser++;
+				}
+			}
+			if (diagTmp.length > 0) {
+				listDiag.add(diagTmp);
+			}
+			diag1 = new IntVar[8];
+		}
+		return listDiag;
 	}
 
 	public boolean validateDataMatrix(JTextField fields[][]) {
